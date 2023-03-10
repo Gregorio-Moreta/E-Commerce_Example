19ede3d95e03793ee4ff0728ce1f2971370246dc
Gregorio-Moreta <78994816+Gregorio-Moreta@users.noreply.github.com>
2023-03-10 04:13:22 -0500

    added minor styling, experimenting typical selectors/ properties to get my bearings

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='footer'>
+    <div className='bg-blue-800 mx-auto lg:max-width: 1024px h-24'>
       Footer
     </div>
   )
